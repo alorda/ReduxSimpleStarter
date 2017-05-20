eedd28e7e5c50eb71b30ee4cb641df064420498e
Jonathan Alorda <jonathan.alorda@gmail.com>
2017-05-19 21:08:25 -0500

    Created Youtube API Key, installed the npm for youtube api search, erased the src directory and starting from scratch.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCJ1GOCZWk7J6E44yZj1zRsgSNFjE8s2xc';
+// Create a new component. This component should produce some HTML
+const App = () => {
+    return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Put that Component's generated HTML into the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
